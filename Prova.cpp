5be90ac097f2032c649df04fc4f2d41629bb96db
marcorax93 <marcorax93@gmail.com>
2018-07-18 18:48:31 +0200

    OpenCV added to save and check images, need to use mat rather than vectors

--- Prova.cpp
+++ Prova.cpp
@@ -6,12 +6,49 @@
 #include <stdlib.h>
 #include <fstream>
 #include <string>
+
 #include <iostream>
 #include <Davisloading.hpp>
+#include <opencv2/opencv.hpp>
+
+
 
 int main(){
+    int picture;
+    int posx = 0;
+    int posy = 0;
+    int checkcounter = 0 ;
+    unsigned short test1;
+    unsigned char test2;
+    cv::Mat M(YDIM, XDIM, CV_32F);
+
     DAVISFrames testobj(TEST_FILE, XDIM, YDIM);
-    std::cout<< testobj.frames.size();
+    std::cout<< testobj.frames.size()<<" extracted Frames."<<std::endl;
+    std::cin>>picture;
+
+    for(int i = 0; i<XDIM*YDIM;i++){
+        M.at<float>(posy,posx)=((float)testobj.frames[picture][i])/(float)65536;
+        test1 = testobj.frames[picture][i];
+        test2 = M.at<float>(posy,posx);
+        posx++;
+        if(posx==XDIM){
+            posx=0;
+            posy++;
+        }
+        checkcounter++;
+    }
+    //std::cout<<"M="<<M<<std::endl;
+    //std::cout<<"Last value:"<<testobj.frames[picture][checkcounter-1]<<std::endl;
+
+    if ( !M.data )
+    {
+        printf("No image data \n");
+        return -1;
+    }
+    cv::namedWindow("Display Image", cv::WINDOW_AUTOSIZE );
+    cv::imshow("Display Image", M);
+    cv::waitKey(0);
+
     return 0;
 }
 
